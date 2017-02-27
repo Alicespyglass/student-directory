cd8f8a3cca5f141f49716240acddc3e34415c272
alicespyglass <alicespyglass@gmail.com>
2017-02-27 04:32:40 +0000

    Added comments to code

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first we print the list of students
 puts "The students of Enders Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -12,6 +13,8 @@ puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
 
+# finaly, we print the total number of students
 print "Overall, we have "
+# it's important that print () doesn't add new linecharacters
 print 11
 puts " great students"
