13208f8d8f7e8a252111ec8446495aa97fb8f71a
alicespyglass <alicespyglass@gmail.com>
2017-02-27 05:17:37 +0000

    Made students hash interactive to receive input from user

--- directory.rb
+++ directory.rb
@@ -1,23 +1,27 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+end
 
-# let's put all students into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleon", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november},
-]
 
 def print_header
   puts "The students of Enders Academy"
   puts "-------------"
 end
+
 
 def print(students)
   students.each do |student|
@@ -25,10 +29,14 @@ def print(students)
   end
 end
 
+
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
+
+
 # nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
